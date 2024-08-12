109b0258f99adc6af447e48ef8675aaa192bbe6e
codenamehj <loveim1212@naver.com>
2024-08-12 23:57:58 +0900

    240812 스택, 큐, 덱 문제풀이

---
+++ src/test240812_StackQueueDeck/Parentheses.java
@@ -0,0 +1,49 @@
+package test240812_StackQueueDeck;
+
+import java.io.*;
+import java.util.*;
+
+class Parentheses {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			sb.append(solve(br.readLine())).append('\n');
+		}
+
+		System.out.println(sb);
+	}
+
+	public static String solve(String s) {
+
+		Stack<Character> stack = new Stack<>();
+
+		for (int i = 0; i < s.length(); i++) {
+
+			char c = s.charAt(i);
+
+			// 여는 괄호일 경우 스택에 넣는다.
+			if (c == '(') {
+				stack.push(c);
+			}
+
+			// 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
+			else if (stack.empty()) {
+				return "NO";
+			}
+			// 그 외의 경우 stack 원소를 pop
+			else {
+				stack.pop();
+			}
+		}
+
+		if (stack.empty()) { // 스택이 비어있으면 VPS
+			return "YES";
+		} else {
+			return "NO";
+		}
+	}
+}
---
+++ src/test240812_StackQueueDeck/Zero.java
@@ -0,0 +1,35 @@
+package test240812_StackQueueDeck;
+
+import java.io.*;
+import java.util.*;
+
+class Zero {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 버퍼리더
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 버퍼라이터
+
+		int K = Integer.parseInt(br.readLine());
+
+		Stack<Integer> intStack = new Stack<Integer>();
+
+		for (int i = 0; i < K; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num == 0) {
+				intStack.pop();
+			} else {
+				intStack.push(num);
+			}
+		}
+
+		int sum = 0;
+		while (!intStack.isEmpty()) {
+			sum += intStack.pop();
+		}
+
+		bw.write(sum + "\n");
+
+		bw.flush(); // 남아있는 데이터를 모두 출력시킴
+		bw.close(); // 스트림을 닫음
+	}
+}
